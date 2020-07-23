6a50944fb8db627c83d7db9358f945256ff2928c
jAntonio <ajhonatan27@gmail.com>
2020-07-23 15:22:01 -0300

    ordenando la estructura de mi repositorio y ejercicio 3.1

---
+++ module01_03_Estructuras_Basicas/modulo1ej3x1/src/main/java/com/globallogic/modulo1ej3x1/TiposPrimitivos.java
@@ -0,0 +1,20 @@
+package com.globallogic.modulo1ej3x1;
+
+public class TiposPrimitivos {
+
+	public static void main(String[] args) {
+		
+		int myNum = 15;
+		char myChar = 'a';
+		boolean myBool = true;
+		float myFloat = 2436.2456f;
+		short myShort = 500;
+		
+		System.out.println(myNum);
+		System.out.println(myChar);
+		System.out.println(myBool);
+		System.out.println(myFloat);
+		System.out.println(myShort);
+	}
+
+}
